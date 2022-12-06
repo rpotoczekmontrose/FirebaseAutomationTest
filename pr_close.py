2f9fbf0489a8e213e5d633a381c81a06a357f022
Rafal Potoczek <rafal.potoczek@montrosesoftware.com>
2022-12-06 16:56:00 +0100

    conditions

--- pr_close.py
+++ pr_close.py
@@ -1,8 +1,5 @@
 from helpers import *
 
-file1 = open("./.github/variables/myvars.env", "r")
-lines = file1.readlines()
-print(lines)
 pr_number = get_pr_number()
 env_name = "PR_" + pr_number
 worker_project_id = os.environ[env_name]
